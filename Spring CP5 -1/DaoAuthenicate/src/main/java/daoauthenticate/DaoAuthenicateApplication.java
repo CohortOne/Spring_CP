0e85c3714b567e035c80d00f02c04ad39ff605f2
sridharguzzu <75741742+sridharguzzu@users.noreply.github.com>
2021-01-26 09:14:46 +0530

    Add files via upload

---
+++ Spring CP5 -1/DaoAuthenicate/src/main/java/daoauthenticate/DaoAuthenicateApplication.java
@@ -0,0 +1,13 @@
+package daoauthenticate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoAuthenicateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoAuthenicateApplication.class, args);
+	}
+
+}
